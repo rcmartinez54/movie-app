8f7f09cd37ad520d9af6252713d9f1c244fff8d0
Raymond Martinez <rcmartinez1854@gmail.com>
2019-08-20 18:20:21 -0700

    added poster img functionality

--- index.js
+++ index.js
@@ -1,28 +1,32 @@
-// API KEY
-// 05f3d0d627b6f6d55cb015ffb7a0a0c1
-// https://api.themoviedb.org/3/movie/550?api_key=05f3d0d627b6f6d55cb015ffb7a0a0c1
-
 'use strict';
 
-// function enterSite() {
-//     $('.js-enter-btn').on('click', function(event) {
-//         event.preventDefault();
-//         $('.main-contain').html(
-//             `<form id="js-form">
-//                 <label for="search-movie">Enter Movie Title</label>
-//                 <input type="text" name="search-movie" id="js-search-movie" placeholder="Enter Movie Title">
-//                 <button type="submit" class="submit-btn">Submit</button>
-//             </form>`
-//         );
-//     });
-// }
+// API KEY = 05f3d0d627b6f6d55cb005ffb7a0a0c1
+
+function callMovieAPI(inputVal) {
+    fetch (`https://api.themoviedb.org/3/search/movie?api_key=05f3d0d627b6f6d55cb015ffb7a0a0c1&query=${inputVal}`)
+    .then(response => response.json())
+    .then(newResponse => displayResults(newResponse))
+    .catch(error => console.log(error))
+}
 
 function submitForm() {
     $('#js-form').submit(function(event) {
         event.preventDefault();
         let inputVal = $('#js-search-movie').val();
         console.log(inputVal);
+        callMovieAPI(inputVal);
     });
+}
+
+function displayResults(newResponse) {
+    console.log(newResponse);
+    $('.response-container').empty();
+    $('.response-container').append(`
+        <h2>${newResponse.results[0].title}</h2>
+        <p>${newResponse.results[0].overview}</p>
+        <img src="http://image.tmdb.org/t/p/w300/${newResponse.results[0].poster_path}"/>
+    `)
+    
 }
 
 function handleFunctions() {
